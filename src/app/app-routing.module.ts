fc9353a34ece25946816d3060c0794905c62f0cd
Xavier Cobos <xcobos@espol.edu.ec>
2024-06-19 23:31:09 -0500

    experimental de planes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { HomeComponent } from './features/home/home.component';
 import { AuthGuard } from './core/guards/auth.guard';
-import { PlanesComponentComponent} from './planes-component/planes-component.component';
+import { PlanesComponent} from './features/planes/planes.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'planes', component: PlanesComponentComponent },
+  { path: 'planes', component: PlanesComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Pagina principal
 ];
 

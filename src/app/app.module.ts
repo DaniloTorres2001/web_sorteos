d8c20a2315af8778153d33d61ce8f0052b6a49ce
DaniloTorres2001 <dantvera@espol.edu.ec>
2024-06-21 16:12:53 -0500

    Fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,9 @@ import { LoginComponent } from './features/auth/login/login.component';
 import { HomeComponent } from './features/home/home.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { PlanesComponent } from './features/planes/planes.component';
-<<<<<<< HEAD
 import { GiveawayComponent } from './features/banners/giveaway/giveaway.component';
 import { PaginaAntiguaComponent } from './features/banners/pagina-antigua/pagina-antigua.component';
-=======
 import { FuncionamientoComponent } from './features/funcionamiento/funcionamiento.component';
->>>>>>> d26fa1da96d9d9c258493a014c712d386bddcda1
 
 @NgModule({
   declarations: [
@@ -21,12 +18,9 @@ import { FuncionamientoComponent } from './features/funcionamiento/funcionamient
     HomeComponent,
     NavbarComponent,
     PlanesComponent,
-<<<<<<< HEAD
     GiveawayComponent,
-    PaginaAntiguaComponent
-=======
+    PaginaAntiguaComponent,
     FuncionamientoComponent
->>>>>>> d26fa1da96d9d9c258493a014c712d386bddcda1
   ],
   imports: [
     BrowserModule,

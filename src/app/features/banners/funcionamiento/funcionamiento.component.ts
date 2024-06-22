a524f056f8565776acdf6fb3fc0524fa613b46d1
Xavier Cobos <xcobos@espol.edu.ec>
2024-06-22 08:34:43 -0500

    Cambios Home
    Ahora no aparece la barra en Como funciona de la misma componente home

--- src/app/features/banners/funcionamiento/funcionamiento.component.ts
+++ src/app/features/banners/funcionamiento/funcionamiento.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-funcionamiento',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./funcionamiento.component.css']
 })
 export class FuncionamientoComponent {
-
+  @Input() mostrarNavbar: boolean = true;
 }

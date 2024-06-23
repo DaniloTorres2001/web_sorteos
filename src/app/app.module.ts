26705db3714a132cdc54f04235755008ab6c75e3
DaniloTorres2001 <dantvera@espol.edu.ec>
2024-06-22 19:03:33 -0500

    Before Merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AuthGuard } from './core/guards/auth.guard';
 import { AppRoutingModule } from './app-routing.module';
@@ -35,7 +36,8 @@ import { UsadasComponent } from './features/banners/usadas/usadas.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [AuthGuard],
   bootstrap: [AppComponent]
